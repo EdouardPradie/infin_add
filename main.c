8f4769b6e195643b7a7b0bfafe3e49a481961348
Arthur Py <arthur.py@epitech.eu>
2021-11-01 13:20:57 +0100

    hasbulla en string

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 */
 
 #include <stdbool.h>
+#include "include/my.h"
 
 int my_infin_add(int argc, char **argv);
 
 bool check_error(int ac, char **av)
 {
-    if (ac < 2 || ac > 2)
+    if (ac != 3)
         return false;
     if (my_str_isnum(av[1]) != 1 || my_str_isnum(av[2]) != 1)
         return false;
@@ -20,6 +21,11 @@ bool check_error(int ac, char **av)
 
 int main(int ac, char **av)
 {
-    my_infin_add(ac, av);
+    if(check_error(ac, av))
+        my_infin_add(ac, av);
+    else {
+        my_putstr("Invalid args\n");
+        return 84;
+    }
     return 0;
 }
